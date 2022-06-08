ccf815870665061c90250ee9abf98178c58514dd
Jack Doran <jcdoran33@gmail.com>
2022-06-08 14:59:46 -0400

    createdinquirer prompt questions, types, names

--- index.js
+++ index.js
@@ -8,7 +8,62 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Briefly describe the application",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter instructions for installation",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "Enter usage information",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Enter contributors/contribution guidelines",
+            name: "contribution",
+        },
+        {
+            type: "input",
+            message: "Enter Test Instructions",
+            name: "test",
+        },
+        {
+            type: "list",
+            message: "Please choose a license type",
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "Enter your Github username",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "Enter your email address",
+            name: "email",
+        },
+    ])
+    .then((response) => {
+        fs.writeFileSync(
+            "index.html",
+            `enter html here when finished`
+        );
+    });
+};
 
 // Function call to initialize app
 init();
