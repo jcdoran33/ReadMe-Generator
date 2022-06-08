61989cd496975039d8528b1a636b043951de52c6
Jack Doran <jcdoran33@gmail.com>
2022-06-08 17:38:36 -0400

    added the license section creation in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,19 +40,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    switch (license) {
-        case "MIT License":
-            return "https://opensource.org/licenses/MIT";
-            break;
-        case "GNU General Public License v3.0":
-            return "https://www.gnu.org/licenses/gpl-3.0";
-            break;
-        case "Mozilla Public License 2.0":
-            return "https://opensource.org/licenses/MPL-2.0";
-            break;
-        case "None":
-            return "";
-    }
+    return "[![License: "+license+"]("+renderLicenseBadge(license)+")]("+renderLicenseLink(license)+")";
 }
 
 // TODO: Create a function to generate markdown for README
